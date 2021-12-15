ed09bc73a180510d3ee1cd92fae23db4905f4f0c
asn96 <papandiay@gmail.com>
2021-12-15 15:31:07 +0000

    update app for JLS edition 3

--- app/src/main/java/com/amad/clubtdsi/Metiers/clubmetier.java
+++ app/src/main/java/com/amad/clubtdsi/Metiers/clubmetier.java
@@ -10,8 +10,8 @@ import java.util.ArrayDeque;
 public class clubmetier {
 
     static final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    public static final DatabaseReference myRefp = database.getReferenceFromUrl("https://clubtdsi.firebaseio.com/club_participant");
-    public static final DatabaseReference myRefa = database.getReferenceFromUrl("https://clubtdsi.firebaseio.com/club_atelier");
+    public static final DatabaseReference myRefp = database.getReferenceFromUrl("https://clubtdsi.firebaseio.com/club_participant_jls_e3");
+    public static final DatabaseReference myRefa = database.getReferenceFromUrl("https://clubtdsi.firebaseio.com/club_atelier_jls_e3");
 
 
 }
